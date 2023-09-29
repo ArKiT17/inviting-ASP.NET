e2093532c6214e257da742978f9fb9b9dc73af54
Max <99168557+ArKiT17@users.noreply.github.com>
2023-09-29 18:05:43 +0200

    Add files via upload

---
+++ dz3/dz3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using dz3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dz3.Controllers {
+	public class HomeController : Controller {
+		public ViewResult Index() {
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = hour < 12 ? "Доброго ранку" : "Добрий день";
+			return View();
+		}
+		[HttpGet]
+		public ViewResult RsvpForm() {
+			return View();
+		}
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest) {
+			if (ModelState.IsValid)
+	            return View("Thanks", guest);
+			else
+				return View();
+        }
+    }
+}
---
+++ dz3/dz3/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dz3.Models {
+	public class GuestResponse {
+		[Required(ErrorMessage ="Будь ласка, введіть своє ім'я")]
+		public string Name { get; set; }
+        [Required(ErrorMessage = "Будь ласка, введіть email")]
+		[RegularExpression(".+\\@.+\\..+", ErrorMessage = "Ви ввели некоректний email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Будь ласка, введіть телефон")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Будь ласка, вкажіть, чи приймите участь у вечірці")]
+        public bool? WillAttend { get; set; }
+	}
+}
